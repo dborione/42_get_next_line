27868bc92c657bde3f290da7c67a6ee32134284f
Donatienne Borione <dborione@shi-r4-p3.s19.be>
2022-11-22 16:34:50 +0100

    redo

--- get_next_line.c
+++ get_next_line.c
@@ -15,19 +15,19 @@
 
 char	*get_next_line(int fd)
 {
-	static int	i;
+	int	i;
 	static char	*stash;
 	char		*buf;
-	int j;
-	int ret;
+	//int j;
+	//int ret;
 
 	if (!fd)
 		return (NULL);
-	if (!i)
-		i = 0;
-	else
-		i++;
-	j = i;
+	//if (!i)
+	i = 0;
+	// else
+	// 	i++;
+	// j = i;
 	if (!stash)
 		stash = "";
 	if (BUFFER_SIZE < 0)
@@ -35,24 +35,41 @@ char	*get_next_line(int fd)
 	buf = malloc(sizeof(*buf) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	while ((ret = read(fd, buf, BUFFER_SIZE)))
+	while (stash[i] != '\n' && read(fd, buf, BUFFER_SIZE))
 	{
-		stash = ft_strjoin(stash, buf);
+		stash = ft_strjoin(buf, stash);
 		//printf("stash:%s\n", stash);
-		while (stash[i])
-		{	
-			printf("%d", ret);
-			printf(":%c ", stash[i]);
-			if ( stash[i] == '\n')
-			{
-				// printf("j:%d ", j);
-				// printf("i:%d ", i);		
-				free (buf);
-				return (ft_substr(stash, j, (i + 1) - j));
-			}
-			i++;
-		}
+		// while (stash[i])
+		// {
+		// 	if (stash[i] == '\n')
+		// 	{
+		// 		free (buf);
+		// 		return (stash);
+		// 	}
+		// 	i++;
+		//}
+		//printf("stash:%s\n", stash);
+		// printf("%d:", ret);
+		// printf("buf:%s:fin\n", buf);
+		// printf("stash:%s:fin\n", stash);
+		
+		// while (stash[i])
+		// {	
+			
+		// 	//printf("%d", ret);
+		// 	//printf(":%c ", stash[i]);
+		// 	if (stash[i] == '\n')
+		// 	{
+		// 		// printf("j:%d ", j);
+		// 		// printf("i:%d ", i);		
+		// 		free (buf);
+		// 		//if (ret < BUFFER_SIZE)
+
+		// 		return (ft_substr(stash, j, (i + 1) - j));
+		// 	}
+		//i++;
+		// }
 	}
 	free (buf);
-	return (NULL);
+	return (stash);
 }
